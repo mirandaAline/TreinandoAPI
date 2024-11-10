acf19078aff5e56959ba9b5f4c3d92cfa79fcfed
Aline <aline372rosa@gmail.com>
2024-11-10 13:22:41 -0300

    initial commit

---
+++ contas_a_pagar_e_receber/models/contas_a_pagar_receber_model.py
@@ -0,0 +1,10 @@
+from shared.database import Base
+from sqlalchemy import Column, Integer, String, Numeric
+
+class ContaPagarRecber(Base):
+    __tablename__ = 'contas_a_pagar_e_receber'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    descricao = Column(String(30))
+    valor = Column(Numeric)
+    tipo = Column(String(30))
--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,7 +1,12 @@
 from decimal import Decimal
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from contas_a_pagar_e_receber.models.contas_a_pagar_receber_model import ContaPagarRecber
+from shared.dependencies import get_db  
+
+
 
 router = APIRouter(prefix="/contas-a-pagar-e-receber")
 
@@ -36,11 +41,13 @@ def listar_contas():
     ]
     
 @router.post("", response_model=ContaPagarReceberResponse, status_code=201)
-def criar_conta(conta: ContaPagarReceberRequest):
-    return ContaPagarReceberResponse(
-            id=3,
-            descricao=conta.descricao,
-            valor=conta.valor,
-            tipo=conta.tipo
-            
-        )
+def criar_conta(conta_a_pagar_e_receber_request: ContaPagarReceberRequest, 
+                db: Session = Depends(get_db))-> ContaPagarReceberResponse :
+    conta_a_pagar_e_receber = ContaPagarRecber(
+        **conta_a_pagar_e_receber_request.dict()
+    )
+    
+    db.add(conta_a_pagar_e_receber)
+    db.commit()
+    db.refresh(nova_conta)
+    return nova_conta
