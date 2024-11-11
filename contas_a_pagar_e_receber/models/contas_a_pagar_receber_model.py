5829d381a825b53cb51a2c76f57813f3b5c1e309
Aline <aline372rosa@gmail.com>
2024-11-10 23:48:14 -0300

    Funções adicionais

--- contas_a_pagar_e_receber/models/contas_a_pagar_receber_model.py
+++ contas_a_pagar_e_receber/models/contas_a_pagar_receber_model.py
@@ -1,7 +1,7 @@
 from shared.database import Base
 from sqlalchemy import Column, Integer, String, Numeric
 
-class ContaPagarRecber(Base):
+class ContaPagarReceber(Base):
     __tablename__ = 'contas_a_pagar_e_receber'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
--- contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -1,21 +1,22 @@
 from decimal import Decimal
-
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from contas_a_pagar_e_receber.models.contas_a_pagar_receber_model import ContaPagarRecber
+from contas_a_pagar_e_receber.models.contas_a_pagar_receber_model import ContaPagarReceber
 from shared.dependencies import get_db  
 
 
 
 router = APIRouter(prefix="/contas-a-pagar-e-receber")
 
-
 class ContaPagarReceberResponse(BaseModel):
     id: int
     descricao: str
     valor: Decimal
     tipo: str
+    
+    class Config:
+        orm_mode = True 
     
 class ContaPagarReceberRequest(BaseModel):
     descricao: str
@@ -24,30 +25,45 @@ class ContaPagarReceberRequest(BaseModel):
     
 
 @router.get("", response_model=list[ContaPagarReceberResponse])
-def listar_contas():
-    return [
-        ContaPagarReceberResponse(
-            id=1,
-            descricao="Aluguel",
-            valor=1000,
-            tipo="Pagar"
-        ),
-        ContaPagarReceberResponse(
-            id=2,
-            descricao="Salario",
-            valor=5000,
-            tipo="Receber"
-        ),
-    ]
+def listar_contas(db: Session = Depends(get_db))-> list[ContaPagarReceberResponse]:
+    return db.query(ContaPagarReceber).all()
+    
     
 @router.post("", response_model=ContaPagarReceberResponse, status_code=201)
-def criar_conta(conta_a_pagar_e_receber_request: ContaPagarReceberRequest, 
+def criar_conta(conta_request: ContaPagarReceberRequest, 
                 db: Session = Depends(get_db))-> ContaPagarReceberResponse :
-    conta_a_pagar_e_receber = ContaPagarRecber(
-        **conta_a_pagar_e_receber_request.dict()
+    nova_conta = ContaPagarReceber(
+        **conta_request.dict()
     )
     
-    db.add(conta_a_pagar_e_receber)
+    db.add(nova_conta)
     db.commit()
     db.refresh(nova_conta)
     return nova_conta
+
+
+@router.put("/{conta_id}", response_model= ContaPagarReceberResponse, status_code =200)
+def atualizar_conta(conta_id: int, conta_request: ContaPagarReceberRequest, db: Session = Depends(get_db))-> ContaPagarReceberResponse:
+    conta = db.query(ContaPagarReceber).filter(ContaPagarReceber.id == conta_id).first()
+    
+    if not conta:
+        raise HTTPException(status_code=404, detail= "Conta não encontrada")
+    
+    for key, value in conta_request.dict().items():
+        setattr(conta, key, value)
+        
+    db.commit()
+    db.refresh(conta)
+    
+    return conta
+
+@router.delete("/{conta_id}", status_code=204)
+def deletar_conta(conta_id: int, db: Session = Depends(get_db)):
+    conta = db.query(ContaPagarReceber).filter(ContaPagarReceber.id == conta_id).first()
+    
+    if conta is None:
+        raise HTTPException(status_code=404, detail="Conta não encontrada")
+
+    db.delete(conta)
+    db.commit()
+    return {"message": "Conta deletada com sucesso"}
